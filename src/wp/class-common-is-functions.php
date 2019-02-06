9feff3aa17d4c90da7021921080e0667bf07bafd
Varun <varunsridharan23@gmail.com>
2019-02-06 17:15:31 +0530

    Added Few Conditions.

--- src/wp/class-common-is-functions.php
+++ src/wp/class-common-is-functions.php
@@ -15,6 +15,7 @@ if ( ! class_exists( 'Varunsridharan\WordPress\WP_Conditional_Logic\Common_Is_Fu
 	 * @since 1.0
 	 */
 	class Common_Is_Functions extends Users {
+
 		/**
 		 * @param array $condition
 		 *
@@ -33,6 +34,126 @@ if ( ! class_exists( 'Varunsridharan\WordPress\WP_Conditional_Logic\Common_Is_Fu
 		 */
 		public static function is_admin( $condition = array() ) {
 			return self::compare_return( $condition, is_admin() );
+		}
+
+		/**
+		 * @param array $condition
+		 *
+		 * @static
+		 * @return bool|mixed
+		 */
+		public static function is_date( $condition = array() ) {
+			return self::compare_return( $condition, is_date() );
+		}
+
+		/**
+		 * @param array $condition
+		 *
+		 * @static
+		 * @return bool|mixed
+		 */
+		public static function is_year( $condition = array() ) {
+			return self::compare_return( $condition, is_year() );
+		}
+
+		/**
+		 * @param array $condition
+		 *
+		 * @static
+		 * @return bool|mixed
+		 */
+		public static function is_month( $condition = array() ) {
+			return self::compare_return( $condition, is_month() );
+		}
+
+		/**
+		 * @param array $condition
+		 *
+		 * @static
+		 * @return bool|mixed
+		 */
+		public static function is_day( $condition = array() ) {
+			return self::compare_return( $condition, is_day() );
+		}
+
+		/**
+		 * @param array $condition
+		 *
+		 * @static
+		 * @return bool|mixed
+		 */
+		public static function is_time( $condition = array() ) {
+			return self::compare_return( $condition, is_time() );
+		}
+
+		/**
+		 * @param array $condition
+		 *
+		 * @static
+		 * @return bool|mixed
+		 */
+		public static function is_new_day( $condition = array() ) {
+			return self::compare_return( $condition, is_new_day() );
+		}
+
+		/**
+		 * @param array $condition
+		 *
+		 * @static
+		 * @return bool|mixed
+		 */
+		public static function is_multi_author( $condition = array() ) {
+			return self::compare_return( $condition, is_multi_author() );
+		}
+
+		/**
+		 * @param array $condition
+		 *
+		 * @static
+		 * @return bool|mixed
+		 */
+		public static function comments_open( $condition = array() ) {
+			return self::compare_return( $condition, comments_open() );
+		}
+
+		/**
+		 * @param array $condition
+		 *
+		 * @static
+		 * @return bool|mixed
+		 */
+		public static function is_post_type_hierarchical( $condition = array() ) {
+			return self::compare_return( $condition, is_post_type_hierarchical( self::get_extra_arg( $condition ) ) );
+		}
+
+		/**
+		 * @param array $condition
+		 *
+		 * @static
+		 * @return bool|mixed
+		 */
+		public static function is_post_type_archive( $condition = array() ) {
+			return self::compare_return( $condition, is_post_type_archive( self::get_extra_arg( $condition ) ) );
+		}
+
+		/**
+		 * @param array $condition
+		 *
+		 * @static
+		 * @return bool|mixed
+		 */
+		public static function is_author( $condition = array() ) {
+			return self::compare_return( $condition, is_author( self::get_extra_arg( $condition ) ) );
+		}
+
+		/**
+		 * @param array $condition
+		 *
+		 * @static
+		 * @return bool|mixed
+		 */
+		public static function is_network_admin( $condition = array() ) {
+			return self::compare_return( $condition, is_network_admin() );
 		}
 
 		/**
@@ -71,6 +192,16 @@ if ( ! class_exists( 'Varunsridharan\WordPress\WP_Conditional_Logic\Common_Is_Fu
 		 * @static
 		 * @return bool|mixed
 		 */
+		public static function is_search( $condition = array() ) {
+			return self::compare_return( $condition, is_search() );
+		}
+
+		/**
+		 * @param array $condition
+		 *
+		 * @static
+		 * @return bool|mixed
+		 */
 		public static function is_attachment( $condition = array() ) {
 			return self::compare_return( $condition, is_attachment() );
 		}
@@ -101,6 +232,16 @@ if ( ! class_exists( 'Varunsridharan\WordPress\WP_Conditional_Logic\Common_Is_Fu
 		 * @static
 		 * @return bool|mixed
 		 */
+		public static function is_single( $condition = array() ) {
+			return self::compare_return( $condition, is_single( self::get_extra_arg( $condition ) ) );
+		}
+
+		/**
+		 * @param array $condition
+		 *
+		 * @static
+		 * @return bool|mixed
+		 */
 		public static function is_single_page( $condition = array() ) {
 			return self::compare_return( $condition, is_single() );
 		}
@@ -122,7 +263,87 @@ if ( ! class_exists( 'Varunsridharan\WordPress\WP_Conditional_Logic\Common_Is_Fu
 		 * @return bool|mixed
 		 */
 		public static function is_sticky( $condition = array() ) {
-			return self::compare_return( $condition, is_sticky() );
+			return self::compare_return( $condition, is_sticky( self::get_extra_arg( $condition ) ) );
+		}
+
+		/**
+		 * @param array $condition
+		 *
+		 * @static
+		 * @return bool|mixed
+		 */
+		public static function is_page( $condition = array() ) {
+			return self::compare_return( $condition, is_page( self::get_extra_arg( $condition ) ) );
+		}
+
+		/**
+		 * @param array $condition
+		 *
+		 * @static
+		 * @return bool|mixed
+		 */
+		public static function is_paged( $condition = array() ) {
+			return self::compare_return( $condition, is_paged() );
+		}
+
+		/**
+		 * @param array $condition
+		 *
+		 * @static
+		 * @return bool|mixed
+		 */
+		public static function is_page_template( $condition = array() ) {
+			return self::compare_return( $condition, is_page_template( self::get_extra_arg( $condition ) ) );
+		}
+
+		/**
+		 * @param array $condition
+		 *
+		 * @static
+		 * @return bool|mixed
+		 */
+		public static function is_category( $condition = array() ) {
+			return self::compare_return( $condition, is_category( self::get_extra_arg( $condition ) ) );
+		}
+
+		/**
+		 * @param array $condition
+		 *
+		 * @static
+		 * @return bool|mixed
+		 */
+		public static function is_tag( $condition = array() ) {
+			return self::compare_return( $condition, is_tag( self::get_extra_arg( $condition ) ) );
+		}
+
+		/**
+		 * @param array $condition
+		 *
+		 * @static
+		 * @return bool|mixed
+		 */
+		public static function has_tag( $condition = array() ) {
+			return self::compare_return( $condition, has_tag( self::get_extra_arg( $condition ) ) );
+		}
+
+		/**
+		 * @param array $condition
+		 *
+		 * @static
+		 * @return bool|mixed
+		 */
+		public static function is_tax( $condition = array() ) {
+			return self::compare_return( $condition, is_tax( self::get_extra_arg( $condition ) ) );
+		}
+
+		/**
+		 * @param array $condition
+		 *
+		 * @static
+		 * @return bool|mixed
+		 */
+		public static function has_term( $condition = array() ) {
+			return self::compare_return( $condition, has_term( self::get_extra_arg( $condition ) ) );
 		}
 	}
 }
